ae30e082cbfe511e58057c0f8305dbd64e422f28
unknown <54732910+facundogaetan@users.noreply.github.com>
2024-09-18 15:38:58 -0300

    inicio de proyecto

---
+++ test/dominio/CompaniaDeTransporteTest.java
@@ -0,0 +1,17 @@
+package dominio;
+
+import org.junit.Test;
+
+public class CompaniaDeTransporteTest {
+	
+	@Test
+	public void dadoQueExisteUnaCompaniaDeTransportePuedoAgregarVehiculos() {
+		
+		//preparacion
+		CompaniaDeTransporte companiaDeTransporte = new CompaniaDeTransporte();
+		//ejecucion
+		
+		//verificacion
+	}
+
+}
